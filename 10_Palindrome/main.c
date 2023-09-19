923b83fdeaa33cb5b9e9a6b9d19a3b806952a608
Naywvi <nagib.lakhdari.pro@gmail.com>
2023-09-19 12:24:36 +0200

    exo

--- 10_Palindrome/main.c
+++ 10_Palindrome/main.c
@@ -5,30 +5,21 @@
 char *reverse(char s[]){
     int lim = strlen(s) - 1;
     char *sreverse = malloc(sizeof(char)*lim+1);
-
     int limTemp = lim;
-    for (unsigned i = 0; i <= lim ; i++)
-    {   
+    for (unsigned i = 0; i <= lim ; i++){   
         sreverse[i] = s[limTemp];
         limTemp--;
         if(limTemp < 0) return sreverse;
     }
-    
     return 0;
 }
 
 int main() {
     int palin = 0;
     char snum[10];
-
     scanf("%d", &palin);
-
     sprintf(snum, "%d", palin);
-    
-    char *sreverse = reverse(snum);
-    
-    if(strcmp(snum, sreverse) == 0) printf("Palindrome");
+    if(strcmp(snum, reverse(snum)) == 0) printf("Palindrome");
     else printf("Not Palindrome");
- 
     return 0;
 }
