5b51e5900488b2b39265bedc7b7ae63030f6398c
Naywvi <nagib.lakhdari.pro@gmail.com>
2023-09-20 08:56:26 +0200

    exo

--- 18_Int_to_binary/main.c
+++ 18_Int_to_binary/main.c
@@ -2,24 +2,16 @@
 #include <stdlib.h> 
 
 int main(){
-	
 	int tab[10], nbr, i;  
-	
 	printf("Number : ");  
 	scanf("%d",&nbr);  
-	
-	for(i=0; nbr > 0; i++)  
-	{  
+	for(i=0; nbr > 0; i++){  
 		tab[i] = nbr%2;  
 		nbr = nbr/2;  
 	} 
-	
 	printf("\nBinary = ");
-	
-	for(i=i-1; i >= 0; i--)  
-	{  
+	for(i=i-1; i >= 0; i--)  {  
 		printf("%d",tab[i]);  
 	}  
-	
 	return 0;
 }
